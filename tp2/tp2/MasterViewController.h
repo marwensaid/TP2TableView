bb51dae30c6bbe51d2e2790808e0b06183348dac
marwensaid <marwensaidi1@gmail.com>
2015-11-04 13:09:01 +0100

    add totally locally project to git repo

---
+++ tp2/tp2/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  tp2
+//
+//  Created by ios on 02/11/2015.
+//  Copyright (c) 2015 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
